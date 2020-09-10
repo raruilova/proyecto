cf499423292efc07e6e1a25e0151528ef0cb61ee
davatem17 <perezd625@gmail.com>
2020-09-10 15:01:41 -0500

    Home

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_user = Role::where('name','user')->first();
+        $role_admin= Role::where('name','admin')->first();
+        $role_jefe= Role::where('name','jefe')->first();
+        
+        $user = new User();
+        $user->name = "User";
+        $user->email = "user@mail.com";
+        $user->password = bcrypt('query');
+        $user->save();
+        $user->roles()->attach($role_user);
+
+
+        $user = new User();
+        $user->name = "Admin";
+        $user->email = "admin@mail.com";
+        $user->password = bcrypt('12345678');
+        $user->save();
+        $user->roles()->attach($role_admin);
+
+
+        $user = new User();
+        $user->name = "Dueño";
+        $user->email = "dueño@mail.com";
+        $user->password = bcrypt('query');
+        $user->save();
+        $user->roles()->attach($role_jefe);
+    }
+}
